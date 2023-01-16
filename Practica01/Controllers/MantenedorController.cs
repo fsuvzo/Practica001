8539ebd9ecbaf73251dff9f9fede844d09f6a1b2
fsuvzo <felipe.suvzo@gmail.com>
2023-01-15 23:56:48 -0300

    Agregue Crear usuario

--- Practica01/Controllers/MantenedorController.cs
+++ Practica01/Controllers/MantenedorController.cs
@@ -40,9 +40,61 @@ namespace Practica01.Controllers
             {
                 _context.Add(usuario);
                 await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(TablaUsuario));
+            }
+            return View(usuario);
+        }
+
+        // GET: Datoes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Usuarios == null)
+            {
+                return NotFound();
+            }
+
+            var dato = await _context.Usuarios.FindAsync(id);
+            if (dato == null)
+            {
+                return NotFound();
+            }
+            return View(dato);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,ApellidoEmail,Estado")] Usuario usuario)
+        {
+            if (id != usuario.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuario);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioExists(usuario.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View(usuario);
+        }
+        private bool UsuarioExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.Id == id);
         }
     }
 }
